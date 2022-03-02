eafb2b2c1f4457e1134cc428b2fd0e1f2b62e000
james-luong <83560549+james-luong@users.noreply.github.com>
2022-03-03 11:53:58 +1300

    commit

--- Project/Test7.py
+++ Project/Test7.py
@@ -1,2 +1,28 @@
-text= [1,2,3]
-print(text)
+class Teacher:
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+        teachers_list.append(self)
+
+    def display_info(self):
+        print('Teacher name:', self.name)
+        print('Teaching class:', self.subject)
+        print('\n***********************************\n')
+
+teachers_list = []
+Teacher('Baker', 'GRA')
+Teacher('Barker', 'MAT')
+Teacher('Graham', 'BIO')
+Teacher('Morgan', 'DTC')
+Teacher('Bell', 'PHY')
+Teacher('Nimmo', 'ART')
+Teacher('McNicol', 'ENG')
+
+teacher_name = ''
+class_code = input('class code: ').upper()
+
+for teacher in teachers_list:
+    for subject in teacher.subject:
+        print(subject)
+
+print(teacher_name)
