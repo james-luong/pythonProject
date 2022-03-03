1a1f84532c576a7d728882dc8b3064521dc2cf1b
james-luong <83560549+james-luong@users.noreply.github.com>
2022-03-03 14:48:54 +1300

    commit

--- Excercises/Baking System/Banking system1.py
+++ Excercises/Baking System/Banking system1.py
@@ -22,33 +22,65 @@ class User:
         print('CC type:', self.cc_type)
         print('Balance:', self.balance)
         print('Account no.:', self.account_no)
-        print('*************************************************')
+        print('*************************************************\n')
 
 def generate_user():
     import csv
     with open ('bankUsers.csv', newline='') as csvfile:
         filereader = csv.reader(csvfile, delimiter=",", quotechar='"')
         for line in filereader:
-            line[8] = line[8].replace('$','') #remove the dollar sign
-            line[8] = line[8].translate({ord(i): None for i in ','}) #remove the comma
+            # line[8] = line[8].replace('$','') #remove the dollar sign
+            # line[8] = line[8].translate({ord(i): None for i in ','}) #remove the comma
             User(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7],
-                 float(line[8]), line[9])
+                 float(line[8][1:]), line[9])
 
 def find_user():
+    is_user = False
     user_name = input('Name: ').title()
-    print()
+
 
     for user in users_list:
-        if user_name in user.first_name or user_name in user.last_name:
-            return user.first_name.display_info()
-    else:
+        if user_name in user.first_name:
+            user.display_info()
+            is_user = True
+        elif user_name in user.last_name:
+            user.display_info()
+            is_user = True
+
+    if not is_user:
         print(f'There are no customers named "{user_name}" in the system.')
-    print()
+
+def overdrafts():
+    total_amount = 0
+
+    for user in users_list:
+        if user.balance < 0:
+            user.display_info()
+            total_amount -= user.balance
+
+    print('Total overdrafts amount: ${:.2f}'.format(total_amount))
+
+def missing_email():
+    count = 0
+
+    for user in users_list:
+        if not user.email:
+            user.display_info()
+            count += 1
+
+    print(f'Total: {count} people')
+
+def bank_details():
+    count = 0
+
+    for user in users_list:
+        count += 1
+
+
 
 #Main
 users_list = []
 generate_user()
+
 for user in users_list:
     user.display_info()
-
-find_user()
